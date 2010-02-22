bb07f686d57bc111e31905bed0b155bde97a1b8b
Andrey Sobolev <sobolev@bio.lmu.de>
2010-02-22 12:36:26 +0000

    New class for Multiple choice fields added

--- apps/fields/models.py
+++ apps/fields/models.py
@@ -2,13 +2,13 @@ from django import forms
 from django.forms.util import ValidationError, ErrorList
 from django.utils.encoding import smart_unicode, force_unicode
 from django.utils.translation import ugettext_lazy as _
-from django.forms.widgets import Select, SelectMultiple, HiddenInput, MultipleHiddenInput, CheckboxSelectMultiple
+from django.forms.widgets import Select, SelectMultiple, HiddenInput, MultipleHiddenInput
 
 # A ModelMultipleChoiceField with "Clear" helptext
 
 class MMCFClearField(forms.ModelChoiceField):
     """A MultipleChoiceField whose choices are a model QuerySet."""
-    widget = CheckboxSelectMultiple #SelectMultiple
+    widget = SelectMultiple
     hidden_widget = MultipleHiddenInput
     default_error_messages = {
         'list': _(u'Enter a list of values.'),
