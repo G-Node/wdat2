7f9e8b7283faf3412bcd6739da0256bd86fabd8c
andrey <sobolev@bio.lmu.de>
2011-03-18 12:51:37 +0100

    small syntax fix

--- apps/datafiles/urls.py
+++ apps/datafiles/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls.defaults import * from django.views.generic.simple import direct_to_template
+from django.conf.urls.defaults import * 
+from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
     # your datafiles
