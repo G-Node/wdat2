48c7b626ca3b2ff9759a31c42692c7b0d64e7ae6
andrey <sobolev@bio.lmu.de>
2011-07-29 10:59:02 +0200

    split JSON builder functions from views

--- apps/neo_api/views.py
+++ apps/neo_api/views.py
@@ -8,6 +8,7 @@ from django.core.exceptions import ObjectDoesNotExist, PermissionDenied, Validat
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
+from neo_api.json_builder import *
 from neo_api.models import *
 from meta import *
 try:
@@ -16,15 +17,6 @@ except ImportError:
     import simplejson as json
 import jsonpickle
 import re
-
-def _clean_attr(_attr):
-    """
-    By default attribute names in meta contain prefix "_" to indicate whether an 
-    attribute is mandatory. This needs to be cleaned up before assigning to the
-    NEO object.
-    """
-    if _attr.startswith("_"): return _attr[1:]
-    return _attr
 
 class HttpResponseBasicJSON(HttpResponse):
     """
@@ -268,6 +260,7 @@ def create_or_update(request, neo_id=None):
     return retrieve(request, "info", obj.neo_id, message, not update)
 
 
+@auth_required
 def retrieve(request, enquery, neo_id, message=None, new=False):
     """
     This is a slave function to retrieve a NEO object by given NEO_ID. Due to 
@@ -285,7 +278,7 @@ def retrieve(request, enquery, neo_id, message=None, new=False):
         return Http404
     assigned = False # this will raise an error if requested data does not exist
     if enquery == "info" or enquery == "full":
-        assigned = _assign_attrs(n, obj) or assigned
+        assigned = assign_attrs(n, obj) or assigned
     if enquery == "data" or enquery == "full":
         params = {}
         try:
@@ -295,15 +288,15 @@ def retrieve(request, enquery, neo_id, message=None, new=False):
         except ValueError, e:
             return HttpResponseBadRequestAPI(e.message)
         try:
-            assigned = _assign_data_arrays(n, obj, **params) or assigned
+            assigned = assign_data_arrays(n, obj, **params) or assigned
         except IndexError, e:
             return HttpResponseBadRequestAPI(e.message)
         except ValueError, e:
             return HttpResponseBadRequestAPI(e.message)
     if enquery == "parents" or enquery == "full":
-        assigned = _assign_parents(n, obj) or assigned
+        assigned = assign_parents(n, obj) or assigned
     if enquery == "children" or enquery == "full":
-        assigned = _assign_children(n, obj) or assigned
+        assigned = assign_children(n, obj) or assigned
     if not assigned:
         return HttpResponseBadRequestAPI(meta_messages["no_enquery_related"] % enquery)
     if new: return HttpResponseCreatedAPI(n, request.user, message)
@@ -347,96 +340,10 @@ def assign(request, neo_id):
     pass
 
 
+@auth_required
 def delete(request, neo_id):
     """
     This is a slave function to delete a NEO object by given NEO_ID.
     """
     pass
-
-#===============================================================================
-
-def _assign_attrs(fake, obj):
-    """
-    Assigns attibutes from NEO to fake object for pickling to JSON.
-    """
-    setattr(fake, "size", getattr(obj, "size"))
-    for _attr in meta_attributes[obj.obj_type]:
-        attr = _clean_attr(_attr)
-        setattr(fake, attr, getattr(obj, attr))
-        if hasattr(obj, attr + "__unit"):
-            setattr(fake, attr + "__unit", getattr(obj, attr + "__unit"))
-    return True # there is always something to assign
-
-
-def _assign_data_arrays(fake, obj, **params):
-    """
-    Assigns data-related attrs from NEO to fake object for pickling to JSON.
-    """
-    assigned = False
-    if meta_data_attrs.has_key(obj.obj_type):
-        for arr in meta_data_attrs[obj.obj_type]:
-            if arr == "waveforms":
-                array = []
-                for wf in obj.waveform_set.all():
-                    w = {
-                        "channel_index": wf.channel_index,
-                        "waveform": {
-                            "data": wf.waveform,
-                            "units": wf.waveform__unit
-                        }
-                    }
-                    if obj.obj_type == "spiketrain":
-                        w["time_of_spike"] = {
-                            "data": wf.time_of_spike_data,
-                            "units": wf.time_of_spike__unit
-                        }
-                    array.append(w)
-            else:
-                if arr == "signal" and params:
-                    data = obj.get_slice(**params)
-                else: data = getattr(obj, arr)
-                array = {"data": data, "units": getattr(obj, arr + "__unit")}
-            setattr(fake, arr, array)
-        assigned = True
-    return assigned
-    
-
-def _assign_parents(fake, obj):
-    """
-    Assigns parents from NEO to fake object for pickling to JSON.
-    """
-    assigned = False
-    obj_type = obj.obj_type
-    if meta_parents.has_key(obj_type):
-        if obj_type == "unit":
-            ids = []
-            r = meta_parents[obj_type][0]
-            parents = getattr(obj, r).all()
-            for p in parents:
-                ids.append(p.neo_id)
-            setattr(fake, r, ids)
-        else:
-            for r in meta_parents[obj_type]:
-                parent = getattr(obj, r)
-                if parent:
-                    setattr(fake, r, parent.neo_id)
-                else:
-                    setattr(fake, r, None)
-        assigned = True
-    return assigned
-
-
-def _assign_children(fake, obj):
-    """
-    Assigns children from NEO to fake object for pickling to JSON.
-    """
-    assigned = False
-    obj_type = obj.obj_type
-    if meta_children.has_key(obj_type):
-        for r in meta_children[obj_type]:
-            ch = [o.neo_id for o in getattr(obj, r + "_set").all()]
-            setattr(fake, r, ch)
-        assigned = True
-    return assigned
-
 
