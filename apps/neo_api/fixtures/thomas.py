0441738c23d601e5b6352c4b4460804db5c57110
Andrey Sobolev <andrey@hal04.g-node.pri>
2011-09-08 15:59:29 +0200

    Thomas data upload algorithm adapted to python 2.5

--- apps/neo_api/fixtures/thomas.py
+++ apps/neo_api/fixtures/thomas.py
@@ -1,5 +1,7 @@
 # add performance testing!!
 
+import sys
+sys.path.append("/data/apps/g-node-portal/neoapi/")
 import django_access
 import unittest
 import datetime as dte
@@ -22,7 +24,7 @@ def convert_to_timeseries(line):
 
 
 def create_thomas():
-    u = User.objects.get(pk=2) # Change user!
+    u = User.objects.get(pk=12) # Change user!
     s_offset = len(Segment.objects.all()) # set the correct segment ID offset up!
     r_offset = len(RecordingChannel.objects.all()) # set the correct recording channel ID offset up!
     b = Block()
@@ -34,52 +36,54 @@ def create_thomas():
     b.save()
     # processing LFP FIX
     flag = math.floor(176*12/10)
-    with open('080707/lfp_fix080707.dat', 'r') as f:
-        for i, l in enumerate(f):
-            if i < 176:
-                s = new_segment(i, b, u) # create new segment
-                for e in range(5): # create 5 random events - for WDAT testing
-                    e = new_event(e, s, u)
-            else:
-                s = Segment.objects.get(id=int(math.fmod(i, 176)) + s_offset)
-            if float(i)/176 == math.floor(float(i)/176):
-                # create new recording channel
-                r = RecordingChannel()
-                r.name = "Channel {0}".format(meta_channels[int(math.floor(float(i)/176))])
-                r.index = meta_channels[int(math.floor(float(i)/176))]
-                r.author = u
-                r.date_created = dte.datetime.now()
-                r.save()
-            # creating analogsignal
-            ts = convert_to_timeseries(l)
-            a = new_signal(i, "FIX", s, r, ts, u)
-            # every 10% of file processed
-            if float(i)/flag == math.floor(float(i)/flag):
-                print "%s percent of LFP FIX processed." % int(math.floor(float(i)/flag) * 10)
+    f = open('080707/lfp_fix080707.dat', 'r')
+    for i, l in enumerate(f):
+        if i < 176:
+            s = new_segment(i, b, u) # create new segment
+            for e in range(5): # create 5 random events - for WDAT testing
+                e = new_event(e, s, u)
+        else:
+            s = Segment.objects.get(id=int(math.fmod(i, 176)) + s_offset)
+        if float(i)/176 == math.floor(float(i)/176):
+            # create new recording channel
+            r = RecordingChannel()
+            r.name = "Channel %d" % meta_channels[int(math.floor(float(i)/176))]
+            r.index = meta_channels[int(math.floor(float(i)/176))]
+            r.author = u
+            r.date_created = dte.datetime.now()
+            r.save()
+        # creating analogsignal
+        ts = convert_to_timeseries(l)
+        a = new_signal(i, "FIX", s, r, ts, u)
+        # every 10% of file processed
+        if float(i)/flag == math.floor(float(i)/flag):
+            print "%s percent of LFP FIX processed." % int(math.floor(float(i)/flag) * 10)
+    f.close()
     # processing LFP SAC
     flag = math.floor(368*12/10)
-    with open('080707/lfp_sac080707.dat', 'r') as f:
-        for i, l in enumerate(f):
-            if i < 368:
-                # create new segment
-                s = new_segment(i, b, u)
-                for e in range(5): # create 5 random events - for WDAT testing
-                    e = new_event(e, s, u)
-            else:
-                s = Segment.objects.get(id=int(math.fmod(i, 176)) + 175 + s_offset)
-            if float(i)/368 == math.floor(float(i)/368):
-                # get the recording channel
-                r = RecordingChannel.objects.get(id=math.floor(float(i)/368) + r_offset)
-            ts = convert_to_timeseries(l)
-            a = new_signal(i, "SAC", s, r, ts, u)
-            # every 10% of file processed
-            if float(i)/flag == math.floor(float(i)/flag):
-                print "%s percent of LFP SAC processed." % int(math.floor(float(i)/flag) * 10)
+    f = open('080707/lfp_sac080707.dat', 'r')
+    for i, l in enumerate(f):
+        if i < 368:
+            # create new segment
+            s = new_segment(i, b, u)
+            for e in range(5): # create 5 random events - for WDAT testing
+                e = new_event(e, s, u)
+        else:
+            s = Segment.objects.get(id=int(math.fmod(i, 176)) + 175 + s_offset)
+        if float(i)/368 == math.floor(float(i)/368):
+            # get the recording channel
+            r = RecordingChannel.objects.get(id=math.floor(float(i)/368) + r_offset)
+        ts = convert_to_timeseries(l)
+        a = new_signal(i, "SAC", s, r, ts, u)
+        # every 10% of file processed
+        if float(i)/flag == math.floor(float(i)/flag):
+            print "%s percent of LFP SAC processed." % int(math.floor(float(i)/flag) * 10)
+    f.close()
     print "Data imported successfully. Bye."
 
 def new_segment(i, b, u):
     s = Segment()
-    s.name = "Trial {0}".format(i)
+    s.name = "Trial %d" % i
     s.filedatetime = dte.datetime.now()
     s.index = i
     s.block = b
@@ -90,7 +94,7 @@ def new_segment(i, b, u):
 
 def new_event(i, s, u):
     e = Event()
-    e.label = "This is a random label #{0}".format(i)
+    e.label = "This is a random label #%d" % i
     e.time = np.random.random()
     e.time__unit = "ms"
     e.segment = s
@@ -101,7 +105,7 @@ def new_event(i, s, u):
 
 def new_signal(i, typ, s, r, ts, u):
     a = AnalogSignal()
-    a.name = "LFP {1} Signal-{0}".format(i, typ)
+    a.name = "LFP %s Signal-%d" % (typ, i)
     a.t_start = 0.0
     a.t_start__unit = "ms"
     a.sampling_rate = 20000 # ???
