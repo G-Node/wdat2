cf315a3fba98a921d2a13cf99b98b8ce662b7384
andrey <sobolev@bio.lmu.de>
2011-11-01 21:56:54 +0100

    split settings and local settings

---
+++ django_access.py
@@ -0,0 +1,52 @@
+import os
+import sys
+
+# just import this module and you may play with Django classes
+SYSTEM_ROOT = os.path.normpath(os.path.join(os.getcwd(), ".."))
+PROJECT_NAME = os.getcwd()[os.getcwd().rfind("/")+1:]
+
+# some path settings - order matters
+to_pythonpath = ( 
+    './',
+    PROJECT_NAME,
+    os.path.join(PROJECT_NAME, 'apps/'),
+    os.path.join(PROJECT_NAME, 'apps/spike_evaluation/'),
+    'lib/python2.6/site-packages/',
+    'lib/python2.6/site-packages/pinax/apps/',
+    'lib/python2.6/site-packages/pinax/',
+)
+
+for p in to_pythonpath:
+    path = os.path.abspath(os.path.join(SYSTEM_ROOT, p))
+    if path not in sys.path:
+        sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = '%s.settings' % PROJECT_NAME
+
+"""
+Examples
+
+1. Password reset
+import django_access
+from django.contrib.auth.models import User
+menz = User.objects.get(username="andrey")
+menz.set_password("pass")
+menz.save()
+
+
+2. Http Request56
+import django_access
+from django.http import HttpRequest
+from django.contrib.auth.models import User
+from neo_api.views import create
+
+r = HttpRequest()
+menz = User.objects.get(username="andrey")
+r.user = menz
+r.method = "POST"
+r._read_started = False
+
+# eh.. does not work
+
+
+"""
