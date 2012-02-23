b49730ffecbc54bc3cdd26c99eaa1f6e0604e396
andrey <sobolev.andrey@gmail.com>
2012-02-23 16:42:54 +0100

    odml library structure had to be changed, done

---
+++ apps/ext/python-odml/base.py
@@ -0,0 +1,359 @@
+#-*- coding: utf-8
+"""
+collects common base functionality
+"""
+import doc
+import posixpath
+import terminology
+import mapping
+
+class _baseobj(object):
+    pass
+
+class baseobject(_baseobj):
+    _format = None
+
+    @property
+    def document(self):
+        if self.parent is None:
+            return None
+        return self.parent.document
+
+    def get_terminology_equivalent(self):
+        return None
+
+    def __eq__(self, obj):
+        """
+        do a deep comparison of this object and its odml properties
+        """
+        # cannot compare totally different stuff
+        if not isinstance(obj, _baseobj):
+            return False
+
+        if not isinstance(self, obj.__class__):
+            return False
+
+        for key in self._format:
+            if getattr(self, key) != getattr(obj, key):
+                return False
+
+        return True
+
+    def __ne__(self, obj):
+        """
+        use the __eq__ function to determine if both objects are equal
+        """
+        return not self == obj
+
+    def clean(self):
+        pass
+
+    def clone(self, children=True):
+        """
+        clone this object recursively (if children is True) allowing to copy it independently
+        to another document. If children is False, this acts as a template cloner, creating
+        a copy of the object without any children
+        """
+        # TODO don't we need some recursion / deepcopy here?
+        import copy
+        obj = copy.copy(self)
+        return obj
+
+    def _reorder(self, childlist, new_index):
+        """
+        move this object in its parent child-list to the position *new_index*
+
+        returns the old index at which the object was found
+        """
+        l = childlist
+        old_index = l.index(self)
+
+        # 2 cases: insert after old_index / insert before
+        if new_index > old_index:
+            new_index += 1
+        l.insert(new_index, self)
+        if new_index < old_index:
+            del l[old_index+1]
+        else:
+            del l[old_index]
+        return old_index
+
+class SafeList(list):
+    def index(self, obj):
+        """
+        find obj in list
+
+        be sure to use "is" based comparison (instead of __eq__)
+        """
+        for i, e in enumerate(self):
+            if e is obj:
+                return i
+        raise ValueError("remove: %s not in list" % repr(obj))
+
+    def remove(self, obj):
+        """
+        remove an element from this list
+
+        be sure to use "is" based comparison (instead of __eq__)
+        """
+        del self[self.index(obj)]
+
+class SmartList(SafeList):
+    def __getitem__(self, key):
+        """
+        provides element index also by searching for an element with a given name
+        """
+        # try normal list index first (for integers)
+        if type(key) is int:
+            return super(SmartList, self).__getitem__(key)
+
+        # otherwise search the list
+        for obj in self:
+            if (hasattr(obj, "name") and obj.name == key) or key == obj:
+                return obj
+
+        # and fail eventually
+        raise KeyError(key)
+
+class sectionable(baseobject, mapping.mapped):
+    def __init__(self):
+        self._sections = SmartList()
+        self._repository = None
+
+    @property
+    def document(self):
+        """
+        returns the parent-most node (if its a document instance) or None
+        """
+        p = self
+        while p.parent:
+            p = p.parent
+        if isinstance(p, doc.Document):
+            return p
+
+    @property
+    def sections(self):
+        return self._sections
+
+    @mapping.remapable_insert
+    def insert(self, position, section):
+        self._sections.append(section)
+        section._parent = self
+
+    @mapping.remapable_append
+    def append(self, section):
+        """adds the section to the section-list and makes this document the section’s parent"""
+        self._sections.append(section)
+        section._parent = self
+
+    def reorder(self, new_index):
+        return self._reorder(self.parent.sections, new_index)
+
+    @mapping.remapable_remove
+    def remove(self, section):
+        self._sections.remove(section)
+        section._parent = None
+
+    def __getitem__(self, key):
+        return self._sections[key]
+
+    def __len__(self):
+        return len(self._sections)
+
+    def __iter__(self):
+        return self._sections.__iter__()
+
+    def itersections(self, recursive=False, yield_self=False):
+        """
+        iterate each child section
+
+        if *recursive* is set, iterate all child sections recurively (depth-search)
+        """
+        if yield_self:
+            yield self
+        for i in self._sections:
+            yield i
+            if recursive:
+                for j in i.itersections(recursive=recursive):
+                    yield j
+
+    def contains(self, obj):
+        """
+        checks if a subsection of name&type of *obj* is a child of this section
+        if so return this child
+        """
+        for i in self._sections:
+            if obj.name == i.name and obj.type == i.type:
+                return i
+
+    def _matches(self, obj, key=None, type=None):
+        """
+        find out
+        * if the *key* matches obj.name (if key is not None)
+        * or if *type* matches obj.type (if type is not None)
+
+        comparisms are caseinsensitive, however both key and type
+        MUST be lower-case.
+        """
+        return (key is None or (key is not None and hasattr(obj, "name") and obj.name == key)) \
+            and (type is None or (type is not None and hasattr(obj, "type") and obj.type.lower() == type))
+
+    def find_by_path(self, path):
+        """
+        find a Section/Property through a path like "name1/name2"
+        """
+        path = path.split("/")
+        return self._find_by_path(path)
+
+    def _find_by_path(self, path):
+        """
+        find a Section/Property through a path like ("name1", "name2")
+        """
+        cur = self
+        for i in path:
+            if i == "." or i == "": continue
+            if i == "..":
+                cur = cur.parent
+                continue
+            cur = cur[i]
+        return cur
+
+    def find(self, key=None, type=None, findAll=False):
+        """return the first subsection named *key* of type *type*"""
+        ret = []
+        if type:
+            type = type.lower()
+
+        for s in self._sections:
+            if self._matches(s, key, type):
+                if findAll:
+                    ret.append(s)
+                else:
+                    return s
+        if ret:
+            return ret
+
+    def find_related(self, key=None, type=None, children=True, siblings=True, parents=True, recursive=True, findAll=False):
+        """
+        finds a related section named *key* and/or *type*
+
+          * by searching its children’s children if *children* is True
+            if *recursive* is true all leave nodes will be searched
+          * by searching its siblings if *siblings* is True
+          * by searching the parent element if *parents* is True
+            if *recursive* is True all parent nodes until the root are searched
+          * if *findAll* is True, returns a list of all matching objects
+        """
+        ret = []
+        if type:
+            type = type.lower()
+
+        if children:
+            for section in self._sections:
+                if self._matches(section, key, type):
+                    if findAll:
+                        ret.append(section)
+                    else:
+                        return section
+
+                if recursive:
+                    obj = section.find_related(key, type, children, siblings=False, parents=False, recursive=recursive, findAll=findAll)
+                    if obj is not None:
+                        if findAll:
+                            ret += obj
+                        else:
+                            return obj
+
+        if siblings and self.parent is not None:
+            obj = self.parent.find(key, type, findAll)
+            if obj is not None:
+                if findAll:
+                    ret += obj
+                else:
+                    return obj
+
+        if parents:
+            obj = self
+            while obj.parent is not None:
+                obj = obj.parent
+                if self._matches(obj, key, type):
+                    if findAll:
+                        ret.append(obj)
+                    else:
+                        return obj
+                if not recursive: break
+
+        if ret:
+            return ret
+        return None
+
+    def get_path(self):
+        """
+        returns the absolute path of this section
+        """
+        node = self
+        path = []
+        while node.parent is not None:
+            path.insert(0, node.name)
+            node = node.parent
+        return "/" + "/".join(path)
+
+    @staticmethod
+    def _get_relative_path(a, b):
+        """
+        returns a relative path for navigation from dir *a* to dir *b*
+
+        if the common parent of both is "/", return an absolute path
+        """
+        a += "/"
+        b += "/"
+        parent = posixpath.dirname(posixpath.commonprefix([a,b]))
+        if parent == "/": return b[:-1]
+
+        a = posixpath.relpath(a, parent)
+        b = posixpath.relpath(b, parent)
+        if a == ".": return b
+
+        return posixpath.normpath("../" * (a.count("/")+1) + b)
+
+    def get_relative_path(self, section):
+        """
+        returns a relative (file)path to point to section (e.g. ../other_section)
+
+        if the common parent of both sections is the document (i.e. /), return an absolute path
+        """
+        a = self.get_path()
+        b = section.get_path()
+        return self._get_relative_path(a,b)
+
+    def clean(self):
+        for i in self:
+            i.clean()
+
+    def clone(self, children=True):
+        """
+        clone this object recursively allowing to copy it independently
+        to another document
+        """
+        obj = super(sectionable, self).clone(children)
+        obj._parent = None
+        obj._sections = SmartList()
+        if children:
+            for s in self._sections:
+                obj.append(s.clone())
+
+        return obj
+
+    @property
+    def repository(self):
+        return self._repository
+
+    @repository.setter
+    def repository(self, url):
+        if not url: url = None
+        self._repository = url
+        if url:
+            terminology.deferred_load(url)
+
+    def get_repository(self):
+        return self._repository
---
+++ apps/ext/python-odml/types.py
@@ -0,0 +1,190 @@
+"""
+Provides functionality for validation of the data-types specified
+for odml
+"""
+
+import sys
+self = sys.modules[__name__].__dict__
+
+from datetime import datetime, date, time
+import binascii
+import md5
+
+types = ['string', 'int', 'text', 'float', 'URL', 'datetime', 'boolean', 'date', 'binary', 'person', 'time']
+
+def valid_type(dtype):
+    """
+    checks if *dtype* is a valid type
+    """
+    if dtype in types: return True
+    if dtype is None: return True
+    if dtype.endswith("-tuple"):
+        try:
+            count = int(dtype[:-6])
+            return True
+        except ValueError:
+            pass
+    return False
+
+# TODO also take encoding into account
+def validate(string, dtype):
+    """
+    checks if:
+
+     * *dtype* is a valid type
+     * *string* is a valid expression of type *dtype*
+    """
+    try:
+        if not dtype in types:
+            if dtype.endswith("-tuple"):
+                count = int(dtype[:-6])
+                #try to parse it
+                tuple_get(string, count=count)
+                return True
+            return False
+        #try to parse it
+        self.get(dtype+"_get", str_get)(string)
+    except: #any error, this type ain't valid
+        return False
+
+def get(string, dtype=None, encoding=None):
+    """
+    convert *string* to the corresponding *dtype*
+    """
+    if not dtype: return str_get(string)
+    if dtype.endswith("-tuple"): # special case, as the count-number is included in the type-name
+        return tuple_get(string)
+    if dtype == "binary":
+        return binary_get(string, encoding)
+    return self.get(dtype+"_get", str_get)(string)
+
+def set(value, dtype=None, encoding=None):
+    """
+    serialize a *value* of type *dtype* to a unicode string
+    """
+    if not dtype: return str_set(value)
+    if dtype.endswith("-tuple"):
+        return tuple_set(value)
+    if dtype == "binary":
+        return binary_set(value, encoding)
+    return self.get(dtype+"_set", str_set)(value)
+
+def int_get(string):
+    if not string: return 0
+    try:
+        return int(string)
+    except ValueError:
+        # convert to float first and then cast to int
+        return int(float(string))
+
+def float_get(string):
+    if not string: return 0.0
+    return float(string)
+
+def str_get(string):
+    return unicode(string)
+
+def str_set(value):
+    return unicode(value)
+
+def time_get(string):
+    if not string: return None
+    return datetime.strptime(string, '%H:%M:%S').time()
+
+def time_set(value):
+    if not value: return None
+    return value.isoformat()
+
+def date_get(string):
+    if not string: return None
+    return datetime.strptime(string, '%Y-%m-%d').date()
+
+date_set = time_set
+
+def datetime_get(string):
+    if not string: return None
+    return datetime.strptime(string, '%Y-%m-%d %H:%M:%S')
+
+def datetime_set(value):
+    if not value: return None
+    return value.isoformat(' ')
+
+def boolean_get(string):
+    if not string: return None
+    string = string.lower()
+    truth = ["true", "t", "1"] # be kind, spec only accepts True / False
+    if string in truth: return True
+    false = ["false", "f", "0"]
+    if string in false: return False
+    raise ValueError("Cannot interpret '%s' as boolean" % string)
+
+def boolean_set(value):
+    if value is None: return None
+    return str(value)
+
+def tuple_get(string, count=None):
+    """
+    parse a tuple string like "(1024;768)" and return strings of the elements
+    """
+    if not string: return None
+    string = string.strip()
+    assert string.startswith("(") and string.endswith(")")
+    string = string[1:-1]
+    res = string.split(";")
+    if count is not None: # be strict
+        assert len(res) == count
+    return res
+
+def tuple_set(value):
+    if not value: return None
+    return "(%s)" % ";".join(value)
+
+###############################################################################
+# Binary Encoding Stuff
+###############################################################################
+
+class Encoder(object):
+    def __init__(self, encode, decode):
+        self._encode = encode
+        self._decode = decode
+    def encode(self, data):
+        return self._encode(data)
+    def decode(self, string):
+        return self._decode(string)
+
+encodings = {
+    'base64': Encoder(lambda x: binascii.b2a_base64(x).strip(), binascii.a2b_base64),
+    'quoted-printable': Encoder(binascii.b2a_qp, binascii.a2b_qp),
+    'hexadecimal': Encoder(binascii.b2a_hex, binascii.a2b_hex),
+    None: Encoder(lambda x: x, lambda x: x), #identity encoder
+}
+
+def valid_encoding(encoding):
+    return encoding in encodings
+
+def binary_get(string, encoding=None):
+    "binary decode the *string* according to *encoding*"
+    if not string: return None
+    return encodings[encoding].decode(string)
+
+def binary_set(value, encoding=None):
+    "binary encode the *value* according to *encoding*"
+    if not value: return None
+    return encodings[encoding].encode(value)
+
+def calculate_crc32_checksum(data):
+    return "%08x" % (binascii.crc32(data) & 0xffffffff)
+
+def calculate_md5_checksum(data):
+    return md5.md5(data).hexdigest()
+
+checksums = {
+    'crc32': calculate_crc32_checksum,
+    'md5': calculate_md5_checksum,
+}
+def valid_checksum_type(checksum_type):
+    return checksum_type in checksums
+
+def calculate_checksum(data, checksum_type):
+    if data is None: data = ''
+    return checksums[checksum_type](data)
