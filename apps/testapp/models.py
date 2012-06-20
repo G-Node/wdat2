729211d3c4de243c35f79ab2b2e8c95f2b5a5b3e
andrey <sobolev.andrey@gmail.com>
2012-06-20 09:52:30 +0200

    testapp for fake foreign keys, remove pytables comment from apps/datafiles/modelsgit status

--- apps/testapp/models.py
+++ apps/testapp/models.py
@@ -5,14 +5,21 @@ from django.contrib.auth.models import User
 
 from state_machine.models import SafetyLevel, ObjectState
 
+# TODO make the manager reusable, assigning a model to self
 
 class BookFKManager(models.Manager):
+
+    def __init__(self, *args, **kwargs):
+        super(BookFKManager, self).__init__()
+        self.core_filters = kwargs
+
     def get_query_set(self, **kwargs):
         qs = Book.objects.filter( **(self.core_filters) )
 
         if kwargs.has_key('at_time'):
             at_time = kwargs['at_time']
             qs = qs.filter(starts_at__lte = at_time).filter(ends_at__gt = at_time)
+            # TODO check lte and gt - wirklich?
         else:
             qs = qs.filter(ends_at__isnull = True)
 
@@ -23,12 +30,25 @@ class BookFKManager(models.Manager):
 
         return qs
 
+    def select_versioned(self, *args, **kwargs):
+        return self.get_query_set( **kwargs ).select_related(*args, **kwargs)
+
 
 class Author(ObjectState):
+
     name = models.CharField(max_length=100)
-    book = BookFKManager()
+
+    def __init__(self, *args, **kwargs):
+        super(Author, self).__init__(*args, **kwargs)
+        kwargs = { "author_fk": self.local_id }
+        self.book_set = BookFKManager( **kwargs )
 
 
 class Book(ObjectState):
     title = models.CharField(max_length=100)
-    author = models.IntegerField() # fake FK to Author model
+    author_fk = models.IntegerField() # fake FK to Author model
+
+    @property
+    def author(self):
+        return Author.objects.get( local_id = self.author_fk ) # more kwargs here?
+
