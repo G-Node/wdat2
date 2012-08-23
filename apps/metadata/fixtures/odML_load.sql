d10b907e0fd57f265631a1f82a49e712f03a0074
andrey <sobolev.andrey@gmail.com>
2012-08-23 15:24:46 +0200

    add metadata fixtures - terminologies

---
+++ apps/metadata/fixtures/odML_load.sql
@@ -0,0 +1,89 @@
+BASIC SECTIONS
+INSERT INTO `metadata_section` VALUES ('', '10', 'Analysis', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, NULL, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Cell', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, NULL, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Event', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, NULL, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Electrode', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, NULL, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Experiment', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, NULL, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'HardwareProperties', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, NULL, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'HardwareSettings', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, NULL, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Person', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, NULL, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Preparation', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, NULL, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Recording', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, NULL, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Setup', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, NULL, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Stimulus', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, NULL, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Subject', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, NULL, 1, 1, NULL);
+
+
+SUBSECTIONS
+INSERT INTO `metadata_section` VALUES ('', '10', 'Experiment', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 44, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Behavior', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 44, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Electrophysiology', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 44, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Imaging', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 44, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Modeling', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 44, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Psychophysics', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 44, 1, 1, NULL);
+
+INSERT INTO `metadata_section` VALUES ('', '10', 'Amplifier', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 45, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Attenuator', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 45, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'CameraObjective', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 45, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'DataAcquisition', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 45, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Eyetracker', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 45, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Filter', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 45, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Filterset', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 45, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'HardwareItem', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 45, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'ImageAcquisition', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 45, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Lightsource', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 45, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Microscope', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 45, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'MicroscopeObjective', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 45, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Scanner', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 45, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'StimulusIsolator', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 45, 1, 1, NULL);
+
+INSERT INTO `metadata_section` VALUES ('', '10', 'Amplifier', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 46, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Attenuator', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 46, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'CameraObjective', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 46, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'DataAcquisition', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 46, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Eyetracker', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 46, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Filter', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 46, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Filterset', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 46, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'HardwareItem', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 46, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'ImageAcquisition', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 46, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Lightsource', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 46, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Microscope', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 46, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'MicroscopeObjective', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 46, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Scanner', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 46, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'StimulusIsolator', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 46, 1, 1, NULL);
+
+INSERT INTO `metadata_section` VALUES ('', '10', 'DC', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 51, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Gabor', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 51, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Grating', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 51, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Pulse', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 51, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Movie', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 51, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Ramp', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 51, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'RandomDot', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 51, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Sawtooth', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 51, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Sinewave', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 51, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'Squarewave', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 51, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'StimulusChannel', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 51, 1, 1, NULL);
+INSERT INTO `metadata_section` VALUES ('', '10', 'WhiteNoise', '', '2010-07-09 10:41:43', NULL, NULL, NULL, NULL, 51, 1, 1, NULL);
+
+
+
+
+PROPERTIES
+INSERT INTO `metadata_property` VALUES ('', '10', 'Identifier', '', '', 'An identifier of this analysis. This can be a name under which the analysis should be referred to.', '', '2010-05-27 05:43:36', (SELECT id FROM `metadata_section` WHERE `title`='Analysis' AND `is_template`=1 AND `user_custom_id` IS NULL));
+INSERT INTO `metadata_property` VALUES ('', '10', 'Author', '', '', 'The name of the person who did the analysis, e.g. John Doe or Doe, John.', '', '2010-05-27 05:43:36', (SELECT id FROM `metadata_section` WHERE `title`='Analysis' AND `is_template`=1 AND `user_custom_id` IS NULL));
+INSERT INTO `metadata_property` VALUES ('', '10', 'Date', '', '', 'The date of analysis.', '', '2010-05-27 05:43:36', (SELECT id FROM `metadata_section` WHERE `title`='Analysis' AND `is_template`=1 AND `user_custom_id` IS NULL));
+INSERT INTO `metadata_property` VALUES ('', '10', 'Description', '', '', 'A general description of the analysis.', '', '2010-05-27 05:43:36', (SELECT id FROM `metadata_section` WHERE `title`='Analysis' AND `is_template`=1 AND `user_custom_id` IS NULL));
+INSERT INTO `metadata_property` VALUES ('', '10', 'Method', '', '', 'The name of a method used in this analysis. E.g. "power spectrum".', '', '2010-05-27 05:43:36', (SELECT id FROM `metadata_section` WHERE `title`='Analysis' AND `is_template`=1 AND `user_custom_id` IS NULL));
+INSERT INTO `metadata_property` VALUES ('', '10', 'CommandlineCall', '', '', 'The command used to execute the analysis including the command line parameter.', '', '2010-05-27 05:43:36', (SELECT id FROM `metadata_section` WHERE `title`='Analysis' AND `is_template`=1 AND `user_custom_id` IS NULL));
+INSERT INTO `metadata_property` VALUES ('', '10', 'Comment', '', '', 'Specific comments on this analysis.', '', '2010-05-27 05:43:36', (SELECT id FROM `metadata_section` WHERE `title`='Analysis' AND `is_template`=1 AND `user_custom_id` IS NULL));
+INSERT INTO `metadata_property` VALUES ('', '10', 'CodeFileURL', '', '', 'The URL of the applied piece of program code to actually do the analysis.', '', '2010-05-27 05:43:36', (SELECT id FROM `metadata_section` WHERE `title`='Analysis' AND `is_template`=1 AND `user_custom_id` IS NULL));
+INSERT INTO `metadata_property` VALUES ('', '10', 'CodeRevision', '', '', 'If the analysis code is under version control the revision number can be specified using this property.', '', '2010-05-27 05:43:36', (SELECT id FROM `metadata_section` WHERE `title`='Analysis' AND `is_template`=1 AND `user_custom_id` IS NULL));
+INSERT INTO `metadata_property` VALUES ('', '10', 'ConfigFile', '', '', 'The configurations used to analyse the data can be transferred using this property. Using it will blow up the the size of the metadatafile and we recommend to rather define the URL of underlying configuration file.', '', '2010-05-27 05:43:36', (SELECT id FROM `metadata_section` WHERE `title`='Analysis' AND `is_template`=1 AND `user_custom_id` IS NULL));
+INSERT INTO `metadata_property` VALUES ('', '10', 'ConfigFileURL', '', '', 'The URL of the used configuration file used to run the analysis.', '', '2010-05-27 05:43:36', (SELECT id FROM `metadata_section` WHERE `title`='Analysis' AND `is_template`=1 AND `user_custom_id` IS NULL));
+INSERT INTO `metadata_property` VALUES ('', '10', 'DatasetFile', '', '', 'The data analysed ion the described way can be transferred using this property. Using it will blow up the the size of the metadatafile and we recommend to rather define the URL of underlying dataset file.', '', '2010-05-27 05:43:36', (SELECT id FROM `metadata_section` WHERE `title`='Analysis' AND `is_template`=1 AND `user_custom_id` IS NULL));
+INSERT INTO `metadata_property` VALUES ('', '10', 'DatasetFileURL', '', '', 'The URL of a analysed datasetFile.', '', '2010-05-27 05:43:36', (SELECT id FROM `metadata_section` WHERE `title`='Analysis' AND `is_template`=1 AND `user_custom_id` IS NULL));
+INSERT INTO `metadata_property` VALUES ('', '10', 'ResultFile', '', '', 'The results of this analysis can be transferred using this property. Using it willblow up the the size of the metadatafile and we recommend to rather define the URL of the results file.', '', '2010-05-27 05:43:36', (SELECT id FROM `metadata_section` WHERE `title`='Analysis' AND `is_template`=1 AND `user_custom_id` IS NULL));
+INSERT INTO `metadata_property` VALUES ('', '10', 'ResultFileURL', '', '', 'The URL of a results file.', '', '2010-05-27 05:43:36', (SELECT id FROM `metadata_section` WHERE `title`='Analysis' AND `is_template`=1 AND `user_custom_id` IS NULL));
+INSERT INTO `metadata_property` VALUES ('', '10', 'ResultFigure', '', '', 'A figure showing the results. This property is meant to strore the real figure content. This will blow up the the size of the metadatafile and we recommend to rather define the URL of the figure.', '', '2010-05-27 05:43:36', (SELECT id FROM `metadata_section` WHERE `title`='Analysis' AND `is_template`=1 AND `user_custom_id` IS NULL));
+INSERT INTO `metadata_property` VALUES ('', '10', 'PaperDOI', '', '', 'If the results of this analysis have been published one can store the doi of the paper.', '', '2010-05-27 05:43:36', (SELECT id FROM `metadata_section` WHERE `title`='Analysis' AND `is_template`=1 AND `user_custom_id` IS NULL));
+INSERT INTO `metadata_property` VALUES ('', '10', 'PaperURL', '', '', 'If the results of this analysis have been published the URL of the document can be given here', '', '2010-05-27 05:43:36', (SELECT id FROM `metadata_section` WHERE `title`='Analysis' AND `is_template`=1 AND `user_custom_id` IS NULL));
