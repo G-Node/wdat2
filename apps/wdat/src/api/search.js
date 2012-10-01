2d4aa0c209b6ecdfa305ce9d3a987d116dbf630f
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2012-10-01 17:45:03 +0530

    search-bar layout skeleton
    - includes the go button, a fixed width query box which contains a smaller
      text-box and space for the advanced button.
    - no eventing yet.  This is just a design layout.

---
+++ apps/wdat/src/api/search.js
@@ -0,0 +1,53 @@
+// ------------------ file: search_bar.js ---------//
+
+// Initialize the module WDAT.widgets if it doesn't exist.
+if (!window.WDAT) window.WDAT = {};
+if (!window.WDAT.api) window.WDAT.api = {};
+
+/* VSearchBar. */
+
+WDAT.api.VSearchBar = function(name, bus) {
+  // Initialize name and the container element
+  if (typeof name === 'string') {  // name is a string
+    this._container = $('<div class="search-bar"></div>').attr('id', name);
+    this.name = name;
+  } else if (typeof name === 'object') { // assume it is a jQuery object
+    this._container = name;
+    this._container.addClass('search-bar');
+    this.name = name.attr('id');
+  }
+
+  // There is a requirement for a global bus as well as a local bus.
+  this.bus = bus;
+  this.lBus = new WDAT.api.EventBus();
+  
+  // Create the query div. Contains text-field and the advanced options
+  // handle.
+  var querydiv = $('<div class="query"></div>')
+    , textbox  = $('<input type="text" class="textbox" value="I tried so hard and got so far.  Till the end, it doesnt matter"></input>');
+
+  $(querydiv).append(textbox);
+
+  this._container.append(querydiv);
+
+
+  // Create the go button
+  var gobutton = new WDAT.api.Button('Go', this.lBus, 'go', 'blue');
+  gobutton.toJQ().addClass('go');
+
+  this._container.append(gobutton.toJQ());
+};
+
+(function () {
+  /* Returns the container div as a jQuery object.
+   * Use this method to include the search bar into your document.
+   *
+   * Return value:
+   *  jQuery Object
+   */ 
+  WDAT.api.VSearchBar.prototype.toJQ = function () {
+    return this._container;
+  };
+})();
+
+
