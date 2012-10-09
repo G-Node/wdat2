4495dcd1f67431d157623e08cf0de05956fa2aa8
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2012-10-09 09:08:01 +0200

    Manual merge button.less

--- apps/wdat/src/version.js
+++ apps/wdat/src/version.js
@@ -3,8 +3,4 @@
 // Create the WDAT object if it doesn't exist
 if (!window.WDAT) { window.WDAT = {}; }
 
-<<<<<<< HEAD
 wdat.version = '0.3.2';
-=======
-wdat.version = '0.3.1';
->>>>>>> list-implementation
