a3c02b2654d546aa8ad37b840f912d0fdec89e21
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2012-11-05 15:11:59 +0100

    Adapter works, but not for metadata

--- apps/wdat/src/api/data/resource_adapter.js
+++ apps/wdat/src/api/data/resource_adapter.js
@@ -23,8 +23,7 @@ if (!WDAT.api) WDAT.api = {};
 if (!WDAT.api.data) WDAT.api.data = {};
 
 WDAT.api.data.GNodeResourceAdapter = function() {
-  // nothing to be done here
-  this.x = 'y';
+  
 };
 
 // define methods of ResourceAdapter
@@ -36,25 +35,109 @@ WDAT.api.data.GNodeResourceAdapter = function() {
    * 
    * { 
    *   id: <id>,                // path part of the permalink
+   *   type: <type>,            // e.g. segment, block, section etc.
+   *   category: <cat>,         // data or metadata
    *   name: <name>,
    *   date_created: <date>,
    *   owner: <str>,            // id to the owner
    *   safety_level: <int>,     // 1 = public, 2 = friendly, 3 = private
-   *   ... ,                    // other optional attributes 
+   *   fields: {},              // other optional attributes 
    *   children: {},            // all child objects as a list of ids
    *   parents: {},             // all parent objects as a list of ids
    *   data: {},                // data as key value pairs
    * }
    */
   WDAT.api.data.GNodeResourceAdapter.prototype.adapt = function(data) {
+    var adapted_data = [];
     if (typeof data  === 'string') data = JSON.parse(data);
-    return data;
+    // iterate over results
+    for (var index in data.selected) {
+      var element = data.selected[index];
+      // the adapted result
+      var adapted = {}
+      // adapt general data
+      var tmp = _stripURL(element.permalink).split('/');
+      adapted.id = tmp.join('/');
+      adapted.type = tmp[1];
+      adapted.category = tmp[0] === 'electrophysiology' ? 'data' : 'metadata';
+      if (adapted.category === 'data' && DATA_OBJECTS.data.plotable[adapted.type])
+        adapted.plotable = true;
+      else
+        adapted.plotable = false;
+      adapted.date_created = element.fields.date_created;
+      adapted.owner = _stripURL(element.fields.owner);
+      switch (element.fields.safety_level) {
+        case 1:
+          adapted.safety_level = 'public'
+          break;
+        case 2:
+          adapted.safety_level = 'friendly'
+          break;
+        default:
+          adapted.safety_level = 'private'
+          break;
+      }
+      // set template
+      var template;
+      if (adapted.category === 'data') {
+        if (adapted.plotable)
+          template = DATA_OBJECTS.data.plotable[adapted.type]
+        else
+          template = DATA_OBJECTS.data.container[adapted.type]
+      } else {
+        template = DATA_OBJECTS.metadata[adapted.type]
+      }
+      if (template) {
+        // adapt fields
+        adapted.fields = {}
+        for (var f in template.fields) {
+          f = template.fields[f];
+          if (f === 'name' || (f === 'value' && adapted.type === 'value')) {
+            adapted.name = element.fields[f];
+          } else {
+            adapted.fields[f] = element.fields[f];
+          }
+        }
+        // adapt children
+        adapted.children = {};
+        for (var c in template.children) {
+          c = template.children[c];
+          if (element.fields[c] && element.fields[c].length > 0) {
+            adapted.children[c] = [];
+            for (var i in element.fields[c]) {
+              adapted.children[c][i] = _stripURL(element.fields[c][i]);
+            }
+          }
+        }
+        // adapt parents
+        adapted.parents = {};
+        for (var p in template.parents) {
+          p = template.parents[p];
+          if (element.fields[p]) {
+            adapted.parents[p] = _stripURL(element.fields[p]);
+          }
+        }
+        // adapt data
+        adapted.data = {}
+        for (var d in template.data) {
+          d = template.data[d];
+          if (element.fields[d]) {
+            adapted.data[d] = element.fields[d];
+            if (adapted.data[d].data && typeof adapted.data[d].data === 'string')
+              adapted.data[d].data = _stripURL(adapted.data[d].data);
+          }
+        }
+      }
+      adapted_data.push(adapted);
+    }
+    return adapted_data;
   };
-  
+
   /* Extracts only the path part of a url.
    * 
    */
-  WDAT.api.data.GNodeResourceAdapter.prototype._stripURL = function(url) {
+  WDAT.api.data.GNodeResourceAdapter.prototype._stripURL = _stripURL;
+  function _stripURL(url) {
     var tmp = url.split('://');
     // remove protocol host and port if present
     if (tmp.length > 1) {
@@ -67,22 +150,22 @@ WDAT.api.data.GNodeResourceAdapter = function() {
     // remove parameter
     return tmp.split('?')[0];
   };
-  
+
   // data model description
-  WDAT.api.data.GNodeResourceAdapter.DATA_OBJECTS = {
+  DATA_OBJECTS = {
     metadata: {
       section: {
-          attr: [],
+          fields: [],
           children: [],
           parents: []
         },
       property: {
-          attr: [],
+          fields: [],
           children: [],
           parents: []
         },
       value: {
-          attr: [],
+          fields: [],
           children: [],
           parents: []
         }
@@ -90,94 +173,92 @@ WDAT.api.data.GNodeResourceAdapter = function() {
     data: {
       container: {
         block: {
-          attr: ['name', 'index', 'description', 'file_origin', 'file_datetime', 'rec_datetime'],
-          children: ['segment', 'recordingchannelgroup'],
+          fields: ['name', 'index', 'description', 'file_origin', 'filedatetime', 'recdatetime'],
+          children: ['segment_set', 'recordingchannelgroup_set'],
           parents: []
         },
         segment: {
-          attr: ['name', 'index', 'description', 'file_origin', 'file_datetime', 'rec_datetime'],
+          fields: ['name', 'index', 'description', 'file_origin', 'filedatetime', 'recdatetime'],
           children: ['analogsignal_set', 'irsaanalogsignal_set', 'analogsignalarray_set', 'spiketrain_set', 
                      'spike_set', 'event_set', 'eventarray_set', 'epoch_set', 'epocharray_set'],
           parents: ['block']
         },
         /* move to plotable ? */
         eventarray: {
-          attr: ['name', 'labels', 'description', 'file_origin'],
+          fields: ['name', 'labels', 'description', 'file_origin'],
           data: ['times'],
-          children: ['event'],
+          children: ['event_set'],
           parents: ['segment']
         },
         /* move to plotable ? */
         epocharray: {
-          attr: ['name', 'labels', 'description', 'file_origin'],
+          fields: ['name', 'labels', 'description', 'file_origin'],
           data: ['times', 'durations'],
-          children: ['epoch'],
+          children: ['epoch_set'],
           parents: ['segment']
         },
         /* move to plotable ? */
         analogsignalarray: {
-          attr: ['name', 'description', 'file_origin', 'sampling_rate', 't_start'],
-          data: ['analogsignal_set'],
-          children: ['analogsignal'],
+          fields: ['name', 'description', 'file_origin'],
+          data: ['analogsignal_set', 'sampling_rate', 't_start'],
+          children: ['analogsignal_set'],
           parents: ['segment']
         },
         unit: {
-          attr: ['name', 'description', 'file_origin'],
-          children: ['spiketrain','spike'],
+          fields: ['name', 'description', 'file_origin'],
+          children: ['spiketrain_set','spike_set'],
           parents: ['recordingchannel']
         },
         recordingchannel: {
-          attr: ['name', 'description', 'file_origin', 'coordinate', 'index'],
-          children: ['unit', 'analogsignal', 'irsaanalogsignal'],
+          fields: ['name', 'description', 'file_origin', 'coordinate', 'index'],
+          children: ['unit_set', 'analogsignal_set', 'irsaanalogsignal_set'],
           parents: ['recordingchannelgroup']
         },
         recordingchannelgroup: {
-          attr: ['name', 'description', 'file_origin', 'channel_names', 'channel_indexes'],
-          children: ['recordingchannel', 'analogsignalarray'],
+          fields: ['name', 'description', 'file_origin', 'channel_names', 'channel_indexes'],
+          children: ['recordingchannel_set', 'analogsignalarray_set'],
           parents: ['block']
         }
       },
       plotable: {
         spike: {
-          attr: ['name', 'description', 'file_origin', 'sampling_rate', 'left_sweep'],
-          data: ['time', 'waveform'],
+          fields: ['name', 'description', 'file_origin'],
+          data: ['time', 'waveform', 'sampling_rate', 'left_sweep'],
           children: [],
           parents: ['segment', 'unit']
         },
         spiketrain: {
-          attr: ['name', 'description', 'file_origin', 'sampling_rate', 't_start', 't_stop', 'left_sweep'],
-          data: ['times', 'waveforms'],
+          fields: ['name', 'description', 'file_origin'],
+          data: ['times', 'waveforms', 'sampling_rate', 't_start', 't_stop', 'left_sweep'],
           children: [],
           parents: ['segment', 'unit']
         },
         event: {
-          attr: ['name', 'description', 'file_origin', 'label'],
+          fields: ['name', 'description', 'file_origin', 'label'],
           data: ['time'],
           children: [],
           parents: ['segment', 'eventarray']
         },
         epoch: {
-          attr: ['name', 'description', 'file_origin', 'label'],
+          fields: ['name', 'description', 'file_origin', 'label'],
           data: ['duration', 'time'],
           children: [],
           parents: ['segment', 'epocharray']
         },
         analogsignal: {
-          attr: ['name', 'description', 'file_origin', 'sampling_rate', 't_start'],
-          data: ['signal'],
+          fields: ['name', 'description', 'file_origin'],
+          data: ['signal', 'sampling_rate', 't_start'],
           children: [],
           parents: ['segment', 'analogsignalarray', 'recordingchannel']
         },
         irsaanalogsigal: {
-          attr: ['name', 'description', 'file_origin'],
+          fields: ['name', 'description', 'file_origin'],
           data: ['times', 'samples'],
           children: [],
           parents: ['segment']
         }
-      },
-      all: ''
-    },
-    all: ''
+      }
+    }
   }; // end data model description
-
+  WDAT.api.data.GNodeResourceAdapter.DATA_OBJECTS = DATA_OBJECTS;
 }());
