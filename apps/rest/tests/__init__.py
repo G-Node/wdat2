16df999b23b758bf116b377e0a653aa2cf473b8c
andrey <sobolev.andrey@gmail.com>
2013-03-16 16:38:13 +0100

    more tests added to the REST basic

--- apps/rest/tests/__init__.py
+++ apps/rest/tests/__init__.py
@@ -18,10 +18,10 @@ This module contains abstract classes for testing basic REST functionality for a
 typical REST app. Tests include:
 
 1. CRUD
-2. Security
-3. Bulk update
-4. response mode (full, etc.)
-5. filters
+2. Permissions
+3. response mode (full, etc.)
+4. filters
+5. Etag + last-modified + parent obj update in case of relations change
 
 Inherit these tests in tests module for your custom django app to test 
 REST-fulness of the models. Example:
@@ -47,6 +47,7 @@ class TestBasics(TestGeneric, TestCase):
 >>>
 
 """
+
 
 class TestGeneric( object ):
     """ Base abstract class for testing common REST functions like CRUD etc. 
@@ -102,6 +103,10 @@ class TestGeneric( object ):
 
         return data
 
+    #---------------------------------------------------------------------------
+    # CRUD tests
+    #---------------------------------------------------------------------------
+
     def test_create(self):
         """ creates an object for every model with POST request """
         for model in self.models_to_test:
@@ -130,14 +135,16 @@ class TestGeneric( object ):
 
     def test_update(self):
         """ updates an exising object for every model and validates the success
-        of the update """
+        of the update. expects samples from fixtures """
         for model in self.models_to_test:
 
             post = {} # POST request body to update an object
             obj_type = model().obj_type
-            pk_field = [f for f in model._meta.local_fields if f.primary_key][0]
-            pk = self._get_test_value_for_field( pk_field, self.user )
-            url = "/%s/%s/%d" % (self.app_prefix, obj_type, pk)
+            #pk_field = [f for f in model._meta.local_fields if f.primary_key][0]
+            #pk = self._get_test_value_for_field( pk_field, self.user )
+            #url = "/%s/%s/%d" % (self.app_prefix, obj_type, pk)
+            random_obj = random.choice( model.objects.all( owner=self.user ) )
+            url = "/%s/%s/%d/" % (self.app_prefix, obj_type, random_obj.pk)
 
             attributes = self.backbone[ obj_type ]['attributes']
             data_fields = self.backbone[ obj_type ]['data_fields']
@@ -154,7 +161,7 @@ class TestGeneric( object ):
 
             try:
                 DjangoJSONEncoder().encode(post)
-            except TypeError:
+            except TypeError: # fix the bug in bulk_update too!!
                 import ipdb
                 ipdb.set_trace()
 
@@ -172,7 +179,174 @@ class TestGeneric( object ):
                     str(rdata['selected'][0]['fields'][field.name]),\
                     str( f_value ), "Object: %s, field: %s" % (obj_type, field.name) )
 
+    def test_bulk_update(self):
+        """ test BULK UPDATE. expects samples from fixtures """
 
+        for model in self.models_to_test:
 
+            post = {} # POST request body to update an object
+            obj_type = model().obj_type
+            url = "/%s/%s/" % (self.app_prefix, obj_type)
 
+            attributes = self.backbone[ obj_type ]['attributes']
+            data_fields = self.backbone[ obj_type ]['data_fields']
+            parents = self.backbone[ obj_type ]['parents']
+            required = self.backbone[ obj_type ]['required']
+
+            for attr in attributes + data_fields + parents:
+                field = model._meta.get_field( attr )
+
+                if field.__class__ in self.RANDOM_VALUES.keys():
+                    if attr in required or random.choice([True, False]):
+                        # if not required, 50% to set None
+                        post[ field.name ] = self._get_test_value_for_field( field, self.user )
+
+            response = self.client.post(url + '?bulk_update=1',\
+                DjangoJSONEncoder().encode(post), content_type="application/json")
+            self.assertEqual(response.status_code, 200, \
+                "Obj type %s; response: %s" % (obj_type, response.content))
+
+            response = self.client.get( url )
+            rdata = json.loads(response.content)
+            for f_name, f_value in post.items():
+                field = model._meta.get_field( f_name )
+                field.to_python( f_value ) # just a validation for f_value
+
+                # check only first object
+                self.assertEqual(
+                    str(rdata['selected'][0]['fields'][field.name]),\
+                    str( f_value ), "Object: %s, field: %s" % (obj_type, field.name) )
+
+    def test_delete(self):
+        """ delete object, ensure it's not available anymore, ensure it's 
+        available back in time. expects samples from fixtures """
+
+        # objects should be deleted with a delay to check back in time
+        dt = datetime.now()
+        time.sleep(1)
+
+        for model in self.models_to_test:
+
+            obj_type = model().obj_type
+            random_obj = random.choice( model.objects.all( owner=self.user ) )
+            url = "/%s/%s/%d/" % (self.app_prefix, obj_type, random_obj.pk)
+
+            # delete object
+            response = self.client.delete( url )
+            self.assertEqual(response.status_code, 200, \
+                "Obj type %s; response: %s" % (obj_type, str(response)))
+
+            # try to get deleted object
+            response = self.client.get( url )
+            self.assertEqual(response.status_code, 404, \
+                "Obj type %s; response: %s" % (obj_type, str(response)))
+
+            # try to get object back in time
+            response = self.client.get( url + dt.strftime("%Y-%m-%d %H:%M:%S") )
+            self.assertEqual(response.status_code, 200, \
+                "Obj type %s; response: %s" % (obj_type, str(response)))
+
+    #---------------------------------------------------------------------------
+    # permissions tests
+    #---------------------------------------------------------------------------
+
+    """
+    Here we test that a user can't access private objects, created (with 
+    fixtures) by another user. Tests expect that more than one users are loaded 
+    using fixtures.
+    """
+
+    def test_unauth_create(self):
+        """ test: create an object """
+        """ expected: 401 Unathorized """
+
+        self.client.logout()
+
+        for model in self.models_to_test:
+            obj_type = model().obj_type
+            random_obj = random.choice( model.objects.all() )
+            url = "/%s/%s/" % (self.app_prefix, obj_type)
+
+            response = self.client.post( url, json.dumps(random_obj, \
+                cls=DjangoJSONEncoder), content_type="application/json")
+            self.assertEqual(response.status_code, 401)
+
+    def test_unauth_update(self):
+        """ test: update an object """
+        """ expected: 401 Unathorized """
+
+        self.client.logout()
+
+        for model in self.models_to_test:
+            obj_type = model().obj_type
+            random_obj = random.choice( model.objects.all() )
+            url = "/%s/%s/%d/" % (self.app_prefix, obj_type, random_obj.pk)
+
+            response = self.client.post( url, json.dumps(random_obj, \
+                cls=DjangoJSONEncoder), content_type="application/json")
+            self.assertEqual(response.status_code, 401)
+
+    def test_unauth_get(self):
+        """ test: get single object """
+        """ expected: 401 Unathorized """
+
+        self.client.logout()
+
+        for model in self.models_to_test:
+            obj_type = model().obj_type
+            random_obj = random.choice( model.objects.all() )
+            url = "/%s/%s/%d/" % (self.app_prefix, obj_type, random_obj.pk)
+
+            response = self.client.get( url )
+            self.assertEqual(response.status_code, 401)
+
+    def test_access_alien(self):
+        """ test a user is not be able to access objects, to which he has no 
+        permissions to access """
+        aliens = User.objects.exclude( pk=self.user.pk )
+        alien = random.choice( list(aliens) ) # random user
+
+        for model in self.models_to_test:
+            obj_type = model().obj_type
+
+            # select all unaccessible objects
+            objs = model.objects.exclude( owner=alien, safety_level=1 )
+            random_obj = random.choice(objs)
+            url = "/%s/%s/%d" % (self.app_prefix, obj_type, random_obj.pk)
+
+            response = self.client.get( url )
+            self.assertEqual(response.status_code, 403)
+
+    def test_update_alien(self):
+        """ test a user is not be able to change objects authored by others """
+        aliens = User.objects.exclude( pk=self.user.pk )
+        alien = random.choice( list(aliens) ) # random user
+
+        for model in self.models_to_test:
+            obj_type = model().obj_type
+
+            # select all unaccessible objects
+            objs = model.objects.exclude( owner=alien, safety_level=1 )
+            random_obj = random.choice(objs)
+            url = "/%s/%s/%d" % (self.app_prefix, obj_type, random_obj.pk)
+
+            response = self.client.post( url, json.dumps(random_obj, \
+                cls=DjangoJSONEncoder), content_type="application/json")
+            self.assertEqual(response.status_code, 403)
+
+    #---------------------------------------------------------------------------
+    # response mode tests
+    #---------------------------------------------------------------------------
+
+    def test_response_modes(self):
+        """ test that all response modes (info, link, full, beard) give 
+        appropriate results. for the moment it just tests that there is no 
+        error. TODO: write the real validation for every mode. """
+        for model in self.models_to_test:
+            obj_type = model().obj_type
+            modes = self.serializer.RESPONSE_MODES
+            for m in modes:
+                response = self.client.get("/neo/%s/?q=%s" % (obj_type, m))
+                self.assertEqual(response.status_code, 200, \
+                    "Obj type %s; response: %s" % (obj_type, str(response)))
 
