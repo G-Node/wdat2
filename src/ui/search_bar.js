fffba005eb04f0a6e4c8013c34345efbc2329f26
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2013-05-11 10:32:02 +0200

    SearchBar (unfinished)

---
+++ src/ui/search_bar.js
@@ -0,0 +1,28 @@
+//--------- search_bar.js ---------//
+
+/*
+ * TODO module description.
+ */
+define(['ui/template_container'], function (TemplateContainer) {
+    "use strict";
+
+    function SearchBar(id, bus, search_action, is_active) {
+
+        var _bus = bus;
+
+
+
+    }
+
+    var _SEARCH_BAR_TEMPLATE = '' +
+        '<div class="wdat-search">' +
+        '  <div class="search-top">' + // was top-row
+        '    <textarea class="search-field" rows="2" cols="30"></textarea>' +
+        '    <button class="search-btn"></button>' +
+        '  </div>' + // was bottom-row
+        '  <div class="search-bottom">' +
+        '    <button class="activate-btn"></div>' +
+        '<div class="compose-btn"></div></div></div>';
+
+    return {};
+});
