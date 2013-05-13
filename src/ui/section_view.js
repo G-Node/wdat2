52d96850a8ff0bcfb87ca663995fb672a3f93af3
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2013-05-13 18:13:55 +0200

    Stub for class SectionView

---
+++ src/ui/section_view.js
@@ -0,0 +1,15 @@
+//--------- section_view.js ---------//
+
+/*
+ * TODO module description.
+ */
+define(['util/strings', 'ui/button', 'ui/widget'], function (strings, Button, Widget) {
+    "use strict";
+
+    function SectionView() {
+
+    }
+
+    return SectionView;
+
+});
