d56a126ae3f191eb30c256408c240730575afd2e
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2013-05-14 14:37:18 +0200

    Basic SectionContainer

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 //--------- file: main.js ---------//
 
 
-define(['api/data_api', 'ui/tree', 'ui/list', 'ui/form', 'ui/bread_crumb', 'ui/search_bar', 'ui/tab_folder'], function() {
+define(
+    ['api/data_api','ui/tree','ui/list','ui/form','ui/bread_crumb','ui/search_bar','ui/tab_folder','ui/section_container'],
+    function() {
 
 
 });
---
+++ src/ui/section_container.js
@@ -0,0 +1,64 @@
+//--------- section_container.js ---------//
+
+/*
+ * TODO module description.
+ */
+define(['ui/button', 'ui/template_container'], function (Button, TemplateContainer) {
+    "use strict";
+
+    /**
+     *
+     * @param id
+     * @param bus
+     * @param actions
+     * @param data
+     *
+     * @constructor
+     * @extends {TemplateContainer}
+     * @public
+     */
+    function ModelContainer(id, bus, actions, data) {
+
+        var _bus = bus;
+
+        TemplateContainer.apply(this, [id, _SECTION_TEMPLATE, actions || _ACTIONS, data]);
+
+        /**
+         * @private
+         */
+        this._postprocess = function(jq, data, actions) {
+            // TODO SctionContainer post processing of the template
+        };
+
+        this.refresh();
+
+    }
+
+    var _ACTIONS = ['add', 'edit'];
+
+    var _SECTION_TEMPLATE =  '' +
+        '<div id="<%= this.dom_id %>" class="section-container"><h3>Section</h3>' +
+        '<div class="properties">' +
+        '  <div class="field">' +
+        '    <div class="field-name">Name</div><div class="field-val><%= this.name || unnamed %></div>' +
+        '  </div>' +
+        '  <div class="field">' +
+        '    <div class="field-name">Type</div><div class="field-val><%= this.fields.odml_type || n.a. %></div>' +
+        '  </div>' +
+        '  <div class="field">' +
+        '    <div class="field-name">Description</div>' +
+        '    <div class="field-val><%= this.field.description || n.a. %></div>' +
+        '  </div>' +
+        '  <div class="field">' +
+        '    <div class="field-name">Position</div><div class="field-val><%= this.fields.tree_position || n.a %></div>' +
+        '  </div>' +
+        '  <div class="field">' +
+        '    <div class="field-name">Safety Level</div><div class="field-val><%= this.safety_level || n.a. %></div>' +
+        '  </div>' +
+        '  <div class="field">' +
+        '    <div class="field-name">Creation Date</div><div class="field-val><%= this.date_created || n.a. %></div>' +
+        '  </div>' +
+        '</div></div>';
+
+    return ModelContainer;
+});
