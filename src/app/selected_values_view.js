14b937510941849cf4a90fc8747466cf714ae834
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2013-05-20 19:31:50 +0200

    SelectedValuesView dummy class

--- src/app/selected_values_view.js
+++ src/app/selected_values_view.js
@@ -1,8 +1,8 @@
-//--------- selected_data_view.js ---------//
+//--------- selected_values_view.js ---------//
 
 define(['ui/list'], function(List) {
 
-    function SelectedDataView(html, bus, selSelection, searchEvent) {
+    function SelectedValuesView(html, bus, selSelection, searchEvent) {
         var _html       = $(html) ,
             _bus        = bus ,
             _id         = _html.attr('id') || _bus.uid() ,
@@ -11,19 +11,19 @@ define(['ui/list'], function(List) {
             _actions, _list_actions, _nav, _list;
 
         this._init = function() {
-            _html.addClass('wdat-selected-data-view');
+            _html.addClass('wdat-selected-value-view');
             var list_id = _id + '-data-list';
             _list = new List(list_id, bus, ['sel']);
 
-            _html.append('<h1>Selected Data</h1>');
+            _html.append('<h1>Selected Values</h1>');
             _html.append(_list.jq());
 
-            _list.add({id: 'dummy', name: 'A NEO element'});
+            _list.add({id: 'dummy', name: 'A selected Value'});
         }
 
         this._init();
     }
 
-    return SelectedDataView;
+    return SelectedValuesView;
 
 });
