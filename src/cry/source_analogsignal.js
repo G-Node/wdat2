762719d088988d10be80013e84f8263c40735a3c
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2013-07-12 13:15:17 +0200

    SourceAnalogsignal: now inherits from BasicSource

--- src/cry/source_analogsignal.js
+++ src/cry/source_analogsignal.js
@@ -1,36 +1,47 @@
 //--------- source_analogsignal.js ---------//
 
-define(['util/objects', 'util/strings', 'api/bus'], function(objects, strings, Bus) {
+define(['util/objects', 'util/strings', 'cry/basic_source'],
+    function(objects, strings, BasicSource) {
     "use strict";
 
     var _count = 0;
 
     /**
+     * Source for analogsignals.
      *
-     * @param api DataAPI
-     * @param data Object
+     * @param api
+     * @param data
+     * @param [style]
+     *
      * @constructor
+     * @extends {BasicSource}
+     * @public
      */
     function SourceAnalogsignal(api, data, style) {
 
         var _api = api,
-            _bus, _name, _url, _data, _sliced, _style;
+            _url, _style;
+
+        BasicSource.apply(this, []);
 
         this._init = function() {
-            _style = style || _randStyle();
+            if (style) {
+                _style = style;
+            } else {
+                var rand_style = BasicSource.randomStyle();
+                _style = rand_style.stroke;
+            }
             if (data.type === 'analogsignal' && data.id) {
                 var id = strings.urlToID(data.id);
                 _url  = '/electrophysiology/analogsignal/' + id;
-                _name = objects.deepGet(data, 'name') || 'Analogsignal-' + _count++;
+
+                var name  = objects.deepGet(data, 'name') || 'Analogsignal-' + _count++;
+                this.name(name);
             }
         };
 
-        this.name = function() {
-            return _name;
-        };
-
         this.load = function(callback) {
-            _data = undefined;
+            this.data(null);
             _api.getData(handler, _url, {max_points: 1000});
 
             var that = this;
@@ -50,27 +61,13 @@ define(['util/objects', 'util/strings', 'api/bus'], function(objects, strings, B
                     t += interv;
                 }
 
-                _data = [{data: array, style: _style}];
+                that.data([{data: array, style: _style}]);
                 callback(that)
             }
         };
 
-        this.data = function() {
-            if (!_data) {
-                throw "Source: no data available. Use hasData() to avoid this error.";
-            }
-            return _data;
-        };
-
-        this.dataBorders = function() {
-            if (!_data) {
-                throw "Source: no data available. Use hasData() to avoid this error.";
-            }
-            return _borders(_data);
-        };
-
         this.slice = function(start, end, callback) {
-            _sliced = undefined;
+            this.sliced(null);
 
             var options = {max_points: 1000};
             if (start) options.start = start;
@@ -94,64 +91,13 @@ define(['util/objects', 'util/strings', 'api/bus'], function(objects, strings, B
                     t += interv;
                 }
 
-                _sliced = [{data: array, style: _style}];
+                that.sliced([{data: array, style: _style}]);
                 callback(that);
             }
         };
 
-        this.sliced = function() {
-            if (!_sliced) {
-                throw "Source: no sliced data available. Use hasSliced() to avoid this error.";
-            }
-            return _sliced;
-        };
-
-        this.hasSliced = function() {
-            return (_sliced) ? true : false;
-        };
-
-        this.sliceBorders = function() {
-            if (!_sliced) {
-                throw "Source: no sliced data available. Use hasSliced() to avoid this error.";
-            }
-            return _borders(_sliced);
-        };
 
         this._init();
-    }
-
-    function _borders(array) {
-        /** @type {{xmin: Number, xmax: Number, ymin: Number, ymax: Number}} */
-        var borders = {xmin: Number.MAX_VALUE, xmax: Number.MIN_VALUE,
-                       ymin: Number.MAX_VALUE, ymax: Number.MIN_VALUE};
-
-        for (var i = 0; i < array.length; i += 1) {
-
-            var d = array[i].data;
-
-            for (var j = 1; j < d.length; j += 2) {
-                var x = d[j - 1], y = d[j];
-
-                if (x < borders.xmin)
-                    borders.xmin = x;
-                else if (x > borders.xmax)
-                    borders.xmax = x;
-
-                if (y < borders.ymin)
-                    borders.ymin = y;
-                else if (y > borders.ymax)
-                    borders.ymax = y;
-            }
-        }
-
-        return borders;
-    }
-
-    function _randStyle() {
-        var rand256 = function() {
-            return Math.round(Math.random() * 255);
-        };
-        return 'stroke:rgb(' + rand256() + ',' + rand256() + ',' + rand256() + ')';
     }
 
     return SourceAnalogsignal;
