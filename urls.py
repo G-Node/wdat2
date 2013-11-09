3093d8f43153777e46a37b19cdffd8755c2ff6ea
Andrey Sobolev <andrey@data.g-node.org>
2013-11-09 15:43:33 +0100

    added alias name for the default wdat page

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import url, patterns
 
 urlpatterns = patterns('wdat.views',
-    url(r'^$', 'wdat'),
+    url(r'^$', 'wdat', name='wdat_page'),
     url(r'^wdat/$', 'wdat'),
     url(r'^test/(?P<filename>\w+)(\.html)?$', 'test')
 )
